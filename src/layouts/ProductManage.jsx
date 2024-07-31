052ed1a13745f6e30a8590c0b48f5eee3c9e476f
Quocvan <lyquocvan1999@gmail.com>
2024-07-31 21:22:24 +0700

    update product create

--- src/layouts/ProductManage.jsx
+++ src/layouts/ProductManage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import TableProduct from "../components/TableProduct";
+import { NavLink } from "react-router-dom";
 
 const ProductManage = () => {
   return (
     <div>
       <p>{`Product>List`}</p>
-      <button className="text-white bg-orange-500 py-2 px-3 rounded-md">
+      <NavLink
+        to="/product-management/create-product"
+        className="text-white bg-orange-500 py-2 px-3 rounded-md inline-block"
+      >
         New product
-      </button>
+      </NavLink>
       <div className="mt-2">
         <input
           type="text"
